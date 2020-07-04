bcaf6da402ca95c303af8d7013b899852f74f10d
Mauro Baptista <mauro@carnou.com>
2020-07-04 19:41:57 -0300

    Clean up code and added favicon

--- src/templates/tags.js
+++ src/templates/tags.js
@@ -1,6 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { Link, graphql } from "gatsby"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Bio from "../components/bio"
